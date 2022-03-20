bf25deb57ca352be6cede7386318b0a37117b29c
Birk <birk.johannessen@hotmail.com>
2022-03-20 20:37:07 +0100

    ferdig prosjekt

--- PrimeSpiral.py
+++ PrimeSpiral.py
@@ -2,26 +2,44 @@ import math
 import numpy as np
 import scipy.misc as smp
 import sympy
+from PIL import Image, ImageColor
 
-dimX=1024
-dimY=1024
-
+dimX=7000
+dimY=7000
 origoX=dimX/2
 origoY=dimY/2
 
 
-def main():
-    primes=list(sympy.primerange(0,500))
-    primetall={3,5,7,11,13,17,19}
-    data= np.zeros((dimX,dimY,3), dtype=np.uint8)
+def createPrimeSpiral():
+    primes=list(sympy.primerange(2,int((dimY/2))))
+    img = Image.new('RGB', (dimX,dimY),(0,0,0))
 
-
-    for prime in primetall:
+    for prime in primes:
         x,y=calcXY(prime)
-        data[origoX+x,origoY+y] = [0,0,255]
+        drawPlus(x,y,img, 'white')
 
-    #img = smp.toimage( data )
-    #img.show()
+    img.save('PrimeSpiral.png')
+
+def createNSpiral():
+    N=list(range(0,int(origoY)))
+    img = Image.new('RGB', (dimX,dimY),(0,0,0))
+
+    for n in N:
+        x, y = calcXY(n)
+        if n==397:
+            drawPlus(x,y,img,'red')
+        else:
+            drawPlus(x,y,img,'white')
+
+    img.save('Nspiral.png')
+
+def drawPlus(x,y, img, color):
+    img.putpixel((int(origoX + x), int(origoY + y)), ImageColor.getcolor(color, 'RGB')) #senter
+    img.putpixel((int(origoX + x+1), int(origoY + y)), ImageColor.getcolor(color, 'RGB')) #høyre
+    img.putpixel((int(origoX + x-1), int(origoY + y)), ImageColor.getcolor(color, 'RGB')) #venste
+    img.putpixel((int(origoX + x), int(origoY + y+1)), ImageColor.getcolor(color, 'RGB')) #opp
+    img.putpixel((int(origoX + x), int(origoY + y-1)), ImageColor.getcolor(color, 'RGB')) #ned
+
 
 def calcXY(prime):
     radius = prime
@@ -29,5 +47,14 @@ def calcXY(prime):
     y = radius*math.sin(prime)
     return x,y
 
+def drawOrigo(img, color):
+    img.putpixel((int(origoX), int(origoY)), ImageColor.getcolor(color, 'RGB')) #senter
+
+def drawAxis(img, color):
+    for i in range(0,dimX-1):
+        img.putpixel((int(origoX),int(i)),ImageColor.getcolor(color, 'RGB'))
+        img.putpixel((int(i),int(origoY)),ImageColor.getcolor(color, 'RGB'))
+
 if __name__ == "__main__":
-    main()
+    createPrimeSpiral()
+    createNSpiral()
